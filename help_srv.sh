6cfb696527a2294459055419888ff0920a845222
Dubrovin Egor <dubrovin.en@ya.ru>
2019-10-14 23:44:54 +0300

    Returned to original helper scripts

--- help_srv.sh
+++ help_srv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DRIVER_NAME="$1".ko
+TEST_NAME="$2"
 
 ##########################################
 # Print help
@@ -16,6 +17,8 @@ echo "tftp -g -r libstdc++.so.6 192.168.1.2"
 echo "mv libstdc++.so.6 /lib/libstdc++.so.6"
 echo "tftp -g -r $DRIVER_NAME 192.168.1.2"
 echo "insmod $DRIVER_NAME"
+echo "tftp -g -r $TEST_NAME 192.168.1.2"
+echo "chmod +x $TEST_NAME"
 echo
 echo
 echo "****************************************"
@@ -23,4 +26,5 @@ echo "To update this files on Baikal, use:"
 echo "****************************************"
 echo
 echo "rmmod $DRIVER_NAME && tftp -g -r $DRIVER_NAME 192.168.1.2 && insmod $DRIVER_NAME"
+echo "tftp -g -r $TEST_NAME 192.168.1.2 && ./$TEST_NAME"
 echo
